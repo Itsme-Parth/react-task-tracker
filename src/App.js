67107314ae897ebf06aadbbdf264d93cca5158c1
Parth Chadha <parthc2001@gmail.com>
2021-07-29 23:14:44 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import Header from "./components/Header"
+import Tasks from "./components/Tasks"
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Meeting 1",
+      day: "Feb 6th",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Meeting 2",
+      day: "Feb 6th",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Meeting 3",
+      day: "Feb 6th",
+      reminder: true,
+    },
+  ]);
+
+  // leads to unlimilted re-renders
+  //  setTasks({
+  //   id: 3,
+  //   text: "Meeting 3",
+  //   day: "Feb 6th",
+  //   reminder: true,
+  // });
+
+  //Create Task 
+  
+  //Delete Task
+  const deleteTask = (id) => {
+    console.log("delete", id)
+  }
+
+  // for(let singleTask of tasks)
+  // {
+  //   console.log(singleTask.id, singleTask.reminder);
+  // }
+
+   return (
+    <div className='container'>
+      <Header title = {"Task Tracker"} />
+      <Tasks tasks = {tasks}/>
     </div>
   );
 }
